875edee03525e9ec2880adf46fddb26ed90300df
sidharthdec <sidharthdec@gmail.com>
2018-05-19 17:22:09 +0530

    some changes in properties

---
+++ democonfigclient/democonfigclient/src/main/java/com/example/demo/LicenseRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LicenseRepository extends CrudRepository<License, String>
+
+{
+
+	public List<License> findByOrganizationId(String organizationId);
+
+	public License findByOrganizationIdAndLicenseId(String organizationId, String licenseId);
+
+}
