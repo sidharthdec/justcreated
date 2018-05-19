fa5409e59fe6a704ccadec4f7f1840cf4cb318fb
sidharthdec <sidharthdec@gmail.com>
2018-05-19 19:19:34 +0530

    rest services added

---
+++ demo/adjustmentdemo/src/main/java/com/example/demo/AdjustmentdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdjustmentdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdjustmentdemoApplication.class, args);
+	}
+}
