ff8eb69c8d3ee1302f0bb4cc2377795f43dccd0f
sidharthdec <sidharthdec@gmail.com>
2018-05-19 20:33:21 +0530

    2 rest svc added

---
+++ adjustdemo/src/main/java/com/example/demo/AdjustdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdjustdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdjustdemoApplication.class, args);
+	}
+}
