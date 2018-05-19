0fa91f6de5ac8adbb666737a36e1cb750fd11247
sidharthdec <sidharthdec@gmail.com>
2018-05-19 22:45:18 +0530

    model added , eureka added, eureka clients registeres

---
+++ adjustdemo/src/main/java/com/example/demo/model/Adjustment.java
@@ -0,0 +1,45 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+	@Entity
+	public class Adjustment {
+		
+		@Id
+		@Column(name = "bid")
+		private Long id;
+		
+		@Column(name = "aname")
+		private String name;
+		
+		@Column(name = "cause")
+		private String cause;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getCause() {
+			return cause;
+		}
+
+		public void setCause(String cause) {
+			this.cause = cause;
+		}
+
+	}
